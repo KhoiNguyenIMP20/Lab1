c976ce8d6e7e70898f071beecf765e011750866e
KhoiNguyenIMP20 <93770517+KhoiNguyenIMP20@users.noreply.github.com>
2021-11-10 11:58:05 +0700

    Second Lab Assignment - small modification

--- app/src/main/java/com/example/lab1_nak/AddStudentActivity.kt
+++ app/src/main/java/com/example/lab1_nak/AddStudentActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 class AddStudentActivity : AppCompatActivity() {
@@ -19,14 +20,19 @@ class AddStudentActivity : AppCompatActivity() {
     fun addBtnOnClick(view: View) {
         val name = findViewById<EditText>(R.id.inputStudentName).text.trim()
         val score = findViewById<EditText>(R.id.inputStudentScore).text.trim()
-        val confirmation = findViewById<TextView>(R.id.addStatus)
+        val toast_duration = Toast.LENGTH_SHORT
+        //val confirmation = findViewById<TextView>(R.id.addStatus)
         // Validation process
         if(name.isEmpty() || score.isEmpty()){
-            confirmation.setText(R.string.alert_fill_blanks)
-            confirmation.setTextColor(resources.getColor(R.color.invalid))
+            //confirmation.setText(R.string.alert_fill_blanks)
+            //confirmation.setTextColor(resources.getColor(R.color.invalid))
+            val toast = Toast.makeText(applicationContext,R.string.alert_fill_blanks,toast_duration)
+            toast.show()
         } else {
-            confirmation.setText(R.string.success_add)
-            confirmation.setTextColor(resources.getColor(R.color.success))
+            //confirmation.setText(R.string.success_add)
+            //confirmation.setTextColor(resources.getColor(R.color.success))
+            val toast = Toast.makeText(applicationContext,R.string.success_add,toast_duration)
+            toast.show()
         }
     }
 
